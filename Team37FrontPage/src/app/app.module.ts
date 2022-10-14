f3b9841e49a069734b9b118c8441f8c4a8f520de
Joseph Vallejo <javallej@asu.edu>
2022-10-14 20:54:42 +0200

    BootStrap & Routing

--- Team37FrontPage/src/app/app.module.ts
+++ Team37FrontPage/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
 import { MenuComponent } from './menu/menu.component';
+import {RouterModule, Routes} from "@angular/router";
+import { UsersComponent } from './admin/users/users.component';
+
+const routes: Routes = [
+  {path : 'admin/users', component : UsersComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminComponent,
-    MenuComponent
+    MenuComponent,
+    UsersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
