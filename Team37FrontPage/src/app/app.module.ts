c05451d2654bcb16d4aff88b0894036440241785
Joseph Vallejo <javallej@asu.edu>
2022-10-15 19:39:36 +0200

    Routing // 404 / Links

--- Team37FrontPage/src/app/app.module.ts
+++ Team37FrontPage/src/app/app.module.ts
@@ -6,9 +6,14 @@ import { AdminComponent } from './admin/admin.component';
 import { MenuComponent } from './menu/menu.component';
 import {RouterModule, Routes} from "@angular/router";
 import { UsersComponent } from './admin/users/users.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  {path : 'admin/users', component : UsersComponent}
+  {path : 'admin/users', component : UsersComponent},
+  {path : '', component : HomepageComponent},
+  {path : '404', component : PageNotFoundComponent},
+  {path : '**', redirectTo : '/404'}
 ];
 
 @NgModule({
@@ -16,7 +21,9 @@ const routes: Routes = [
     AppComponent,
     AdminComponent,
     MenuComponent,
-    UsersComponent
+    UsersComponent,
+    HomepageComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
